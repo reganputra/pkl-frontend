4d12b4ae09639a1f55018983c3fab68dad302968
Aqsaprima <aqsaprima11@gmail.com>
2025-03-01 12:37:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+function Homepage() {
+  return (
+    <>
+      <div className="bg-white text-black px-4 py-2 rounded hover:bg-red-500 transition">
+        hello world!!
+      </div>
+      <div></div>
+    </>
+  );
+}
+export default Homepage;
