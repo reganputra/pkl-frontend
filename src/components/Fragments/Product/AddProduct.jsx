4263c254e5a699482425308db1e96a1d7db138f6
Aqsaprima <aqsaprima11@gmail.com>
2025-04-30 13:43:45 +0700

    add image preview to add product form and integrate api for creating new product

--- src/components/Fragments/Product/AddProduct.jsx
+++ src/components/Fragments/Product/AddProduct.jsx
@@ -1,9 +1,69 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
+import axiosInstance from "../../../api/axiosinstance";
 
 function AddProduct({ closeAddProduct }) {
+  const [preview, setPreview] = useState(null);
+  const fileInputRef = useRef();
+  const [formValue, setFormValue] = useState({
+    name: "",
+    kodeBarang: "",
+    quantity: "",
+    category: "",
+    ukuranKemasan: "",
+  });
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+      const previewUrl = URL.createObjectURL(file);
+      setPreview(previewUrl);
+    }
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("image", selectedImage);
+    Object.entries(formValue).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    try {
+      const response = await axiosInstance.post("/items", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      alert("Produk berhasil ditambahkan");
+      console.log("success", response.data);
+
+      setFormValue({
+        name: "",
+        kodeBarang: "",
+        quantity: "",
+        category: "",
+        ukuranKemasan: "",
+      });
+      setSelectedImage(null);
+      setPreview(null);
+      closeAddProduct();
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
+    <div className="fixed inset-0 flex justify-center items-center z-20">
       <div
         className="absolute inset-0 bg-black/40"
         onClick={() => closeAddProduct()}
@@ -12,14 +72,38 @@ function AddProduct({ closeAddProduct }) {
       <form
         action=""
         method="post"
-        className="relative flex flex-col bg-white justify-center items-center text-black border gap-3 w-fit p-8 rounded"
-        onClick={(e) => e.stopPropagation()} // stop click inside form from closing modal
+        onSubmit={handleSubmit}
+        className="relative flex flex-col bg-white justify-center items-center text-black border gap-3 w-4/5 p-8 rounded"
+        onClick={(e) => e.stopPropagation()}
       >
         <FontAwesomeIcon
           icon={faXmark}
           className="flex self-end !h-6 cursor-pointer"
           onClick={() => closeAddProduct()}
         />
+
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+
+        <div
+          onClick={triggerFileInput}
+          className="max-h-40 min-w-20 min-h-20 rounded border-2 cursor-pointer flex items-center justify-center"
+        >
+          {preview ? (
+            <img
+              src={preview}
+              alt="Preview"
+              className="w-full h-full object-cover rounded"
+            />
+          ) : (
+            <span>+</span>
+          )}
+        </div>
 
         <div className="flex gap-3">
           <div className="flex flex-col gap-3">
@@ -29,8 +113,14 @@ function AddProduct({ closeAddProduct }) {
               </label>
               <input
                 type="text"
-                name="NamaProduk"
-                id="NamaProduk"
+                name="name"
+                id="name"
+                onChange={(e) =>
+                  setFormValue((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
                 className="border-2 rounded-lg w-96 h-10 px-2 border-black"
               />
             </div>
@@ -41,8 +131,14 @@ function AddProduct({ closeAddProduct }) {
               </label>
               <input
                 type="text"
-                name="KodeBarang"
-                id="KodeBarang"
+                name="kodeBarang"
+                id="kodeBarang"
+                onChange={(e) =>
+                  setFormValue((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
                 className="border-2 rounded-lg w-96 h-10 px-2 border-black"
               />
             </div>
@@ -55,8 +151,14 @@ function AddProduct({ closeAddProduct }) {
               </label>
               <input
                 type="text"
-                name="Kuantitas"
-                id="Kuantitas"
+                name="quantity"
+                id="quantity"
+                onChange={(e) =>
+                  setFormValue((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
                 className="border-2 rounded-lg w-96 h-10 px-2 border-black"
               />
             </div>
@@ -67,8 +169,14 @@ function AddProduct({ closeAddProduct }) {
               </label>
               <input
                 type="text"
-                name="Kategori"
-                id="Kategori"
+                name="category"
+                id="category"
+                onChange={(e) =>
+                  setFormValue((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
                 className="border-2 rounded-lg w-96 h-10 px-2 border-black"
               />
             </div>
@@ -81,17 +189,24 @@ function AddProduct({ closeAddProduct }) {
           </label>
           <input
             type="text"
-            name="Ukuran"
-            id="Ukuran"
+            name="ukuranKemasan"
+            id="ukuranKemasan"
+            onChange={(e) =>
+              setFormValue((prev) => ({
+                ...prev,
+                [e.target.name]: e.target.value,
+              }))
+            }
             className="border-2 rounded-lg w-96 h-10 px-2 border-black"
           />
         </div>
-
-        <input
+        <button
           type="submit"
-          value="Tambahkan"
-          className="bg-[#BC303E] text-white rounded px-12 py-2 font-bold mt-4"
-        />
+          disabled={!selectedImage || !formValue.name}
+          className="bg-[#BC303E] text-white rounded px-12 py-2 font-bold mt-4 disabled:opacity-50"
+        >
+          Tambahkan
+        </button>
       </form>
     </div>
   );
